1dc26a1826fb437ee0dc84442068f79804e4246f
Viktoras_Dulko <v.dulko@eisgroup.com>
2021-12-10 08:48:41 +0200

    1209

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,14 +1,13 @@
 package guru.springframework;
 
-public class Dollar {
-    int amount;
+public class Dollar extends Money{
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,15 +1,37 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class MoneyTest {
 
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(new Dollar(10), product);
+        product = five.times(3);
+        Assertions.assertEquals(new Dollar(15), product);
     }
+
+    @Test
+    void testEqualityDollar() {
+        Assertions.assertEquals(new Dollar(5), new Dollar(5));
+        Assertions.assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        Assertions.assertEquals(new Franc(10), product);
+        product = five.times(3);
+        Assertions.assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        Assertions.assertEquals(new Franc(5), new Franc(5));
+        Assertions.assertNotEquals(new Franc(5), new Franc(8));
+    }
+
 }
