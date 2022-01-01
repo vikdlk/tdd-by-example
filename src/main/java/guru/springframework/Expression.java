1dd28b756d63867c2747eeda8424c6105c5ea284
Viktoras_Dulko <v.dulko@eisgroup.com>
2022-01-01 22:26:59 +0200

    0101 make it

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -2,13 +2,5 @@ package guru.springframework;
 
 public interface Expression {
 
-    @Override
-    Expression(Money augmend, Money addmend);
-
-    @Override
-    Expression(Money augmend, Money addmend);
-
-    Money reduce(Bank bank, String to);
-
     Money reduce(String to);
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,32 +1,34 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
     void testMultiplication() {
         Money five = Money.dollar(5);
-        Assertions.assertEquals(Money.dollar(10), five.times(2));
-        Assertions.assertEquals(Money.dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
 
         Money fiveF = Money.franc(5);
-        Assertions.assertEquals(Money.franc(10), fiveF.times(2));
+        assertEquals(Money.franc(10), fiveF.times(2));
     }
 
     @Test
     void testEqualityDollar() {
-        Assertions.assertEquals(Money.dollar(5), Money.dollar(5));
-        Assertions.assertNotEquals(Money.dollar(5), Money.dollar(8));
-        Assertions.assertEquals(Money.franc(5), Money.franc(5));
-        Assertions.assertNotEquals(Money.dollar(5), Money.franc(5));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
     void testCurrency() {
-        Assertions.assertEquals("USD", Money.dollar(1).currency());
-        Assertions.assertEquals("CHF", Money.franc(1).currency());
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 
     @Test
@@ -35,16 +37,16 @@ public class MoneyTest {
         Expression sum = five.plus(five);
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
-        Assertions.assertEquals(Money.dollar(10), reduced);
+        assertEquals(Money.dollar(10), reduced);
     }
 
     @Test
-    void testPlusReturnSum() {
+    void testPlusReturnsSum() {
         Money five = Money.dollar(5);
         Expression result = five.plus(five);
         Sum sum = (Sum) result;
-        Assertions.assertEquals(five, sum.augmend);
-        Assertions.assertEquals(five, sum.addmend);
+        assertEquals(five, sum.augmend);
+        assertEquals(five, sum.addmend);
     }
 
     @Test
@@ -52,27 +54,13 @@ public class MoneyTest {
         Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
         Bank bank = new Bank();
         Money result = bank.reduce(sum, "USD");
-        Assertions.assertEquals(Money.dollar(7), result);
+        assertEquals(Money.dollar(7), result);
     }
 
     @Test
     void testReduceMoney() {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
-        Assertions.assertEquals(Money.dollar(1), result);
-    }
-
-    @Test
-    void testReduceMoneyDifferentCurrency() {
-        Bank bank = new Bank();
-        bank.addRate("CHF", "USD", 2);
-        Money result = bank.reduce(Money.franc(2), "USD");
-        Assertions.assertEquals(Money.dollar(1), result);
-    }
-
-    @Test
-    void testIdentityRate() {
-        Assertions.assertEquals(1, new Bank().rate("USD", "USD"));
-        Assertions.assertEquals(1, new Bank().rate("CHF", "CHF"));
+        assertEquals(Money.dollar(1), result);
     }
 }
